d1546526997dfb29972dbf46450750503490f818
Connor Murphy <ConnorLMurphy@gmail.com>
2021-11-29 23:44:25 -0800

    remove all warnings

--- src/CenterContent/ctaContent.js
+++ src/CenterContent/ctaContent.js
@@ -8,7 +8,7 @@ function CTA() {
       <h1 id="contact">Contact</h1>
       <p>Please feel free to request a resume or request a dance</p>
       <a className="CTA-button" href="mailto:dylanpresents@gmail.com?subject=Resume Request!&body=Hey Dylan! Could I please get a copy of your resume?">Request a resume</a>
-      <a className="CTA-button" target="_blank" href="https://giphy.com/search/dance">Request a dance</a>
+      <a className="CTA-button" target="_blank" href="https://giphy.com/search/dance" rel="noreferrer">Request a dance</a>
     </div>
   );
 }
